4f6451d2e7c93574fbc2190a38306990f2612b34
Stanley Wintergreen <kepocnhh@gmail.com>
2024-05-27 14:39:15 +0300

    com.android.tools.build:gradle:8.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,19 @@ import sp.gx.core.buildDir
 import sp.gx.core.buildSrc
 
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
+    repositories.mavenCentral()
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlin}")
-        classpath("com.android.tools.build:gradle:8.2.2")
+        gradle.startParameter.taskRequests.firstOrNull()?.also { ter ->
+            val arg = ter.args.firstOrNull()
+            when {
+                arg == null || "^:?android:\\w+".toRegex().matches(arg) -> {
+                    repositories.google()
+                    classpath("com.android.tools.build:gradle:8.2.2")
+                }
+            }
+        }
     }
 }
 
