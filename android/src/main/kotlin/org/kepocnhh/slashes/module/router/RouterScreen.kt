472d74475c08ca2d453a1df1be483f461a7815f4
Stanley Wintergreen <kepocnhh@gmail.com>
2024-05-28 16:28:20 +0300

    manage external storage

--- android/src/main/kotlin/org/kepocnhh/slashes/module/router/RouterScreen.kt
+++ android/src/main/kotlin/org/kepocnhh/slashes/module/router/RouterScreen.kt
@@ -1,20 +1,85 @@
 package org.kepocnhh.slashes.module.router
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Environment
+import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import org.kepocnhh.slashes.BuildConfig
 import org.kepocnhh.slashes.module.tree.TreeScreen
 
+internal object RouterScreen {
+    enum class PermissionState {
+        REQUESTED,
+        GRANTED,
+    }
+}
+
 @Composable
-internal fun RouterScreen() {
+internal fun RouterScreen(onBack: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White),
     ) {
-        TreeScreen()
+        val activity = LocalContext.current as? Activity
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val permissionState = remember { mutableStateOf<RouterScreen.PermissionState?>(null) }
+        LaunchedEffect(Unit) {
+            lifecycleOwner.lifecycle.currentStateFlow.collect {
+                when (it) {
+                    Lifecycle.State.RESUMED -> {
+                        when (permissionState.value) {
+                            RouterScreen.PermissionState.GRANTED -> {
+                                // noop
+                            }
+                            RouterScreen.PermissionState.REQUESTED -> {
+                                val granted = Environment.isExternalStorageManager()
+                                if (granted) {
+                                    permissionState.value = RouterScreen.PermissionState.GRANTED
+                                } else {
+                                    onBack()
+                                }
+                            }
+                            null -> {
+                                val granted = Environment.isExternalStorageManager()
+                                if (granted) {
+                                    permissionState.value = RouterScreen.PermissionState.GRANTED
+                                } else {
+                                    if (activity == null) TODO()
+                                    val action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
+                                    val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+                                    activity.startActivity(Intent(action, uri))
+                                    permissionState.value = RouterScreen.PermissionState.REQUESTED
+                                }
+                            }
+                        }
+                    }
+                    else -> {
+                        // noop
+                    }
+                }
+            }
+        }
+        when (permissionState.value) {
+            RouterScreen.PermissionState.GRANTED -> {
+                TreeScreen()
+            }
+            else -> {
+                // noop
+            }
+        }
     }
 }
--- android/src/main/kotlin/org/kepocnhh/slashes/module/tree/TreeScreen.kt
+++ android/src/main/kotlin/org/kepocnhh/slashes/module/tree/TreeScreen.kt
@@ -1,10 +1,6 @@
 package org.kepocnhh.slashes.module.tree
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Environment
-import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -18,26 +14,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.collect
 import org.kepocnhh.slashes.App
-import org.kepocnhh.slashes.BuildConfig
 import org.kepocnhh.slashes.util.toPaddings
 import java.io.File
 
@@ -123,65 +109,15 @@ internal fun TreeScreen() {
     ) {
         val logics = App.logics<TreeLogics>()
         val state = logics.state.collectAsState().value
-        val activity = LocalContext.current as? Activity
-        val lifecycleOwner = LocalLifecycleOwner.current
-        val requested = remember { mutableStateOf(false) }
-        println("[Tree|Screen] requested: ${requested.value}") // todo
         LaunchedEffect(Unit) {
-            lifecycleOwner.lifecycle.currentStateFlow.collect {
-                when (it) {
-                    Lifecycle.State.RESUMED -> {
-                        if (state == null) {
-                            val granted = Environment.isExternalStorageManager()
-                            if (granted) {
-                                val type = Environment.DIRECTORY_DOCUMENTS
-                                val current = Environment.getExternalStoragePublicDirectory(type)
-                                    .parentFile ?: TODO()
-                                logics.requestState(current = current)
-                            } else if (!requested.value) {
-                                if (activity == null) TODO()
-                                val action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
-                                val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
-                                activity.startActivity(Intent(action, uri))
-                                requested.value = true
-                            }
-                        }
-                    }
-                    else -> {
-                        // noop
-                    }
-                }
+            if (state == null) {
+                val type = Environment.DIRECTORY_DOCUMENTS
+                val current = Environment.getExternalStoragePublicDirectory(type)
+                    .parentFile ?: TODO()
+                logics.requestState(current = current)
             }
         }
-        val granted = Environment.isExternalStorageManager()
-        if (!granted) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.Center),
-            ) {
-                BasicText(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(64.dp)
-                        .wrapContentSize(),
-                    text = "no permission",
-                )
-                BasicText(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(64.dp)
-                        .clickable {
-                            if (activity == null) TODO()
-                            val action = Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION
-                            val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
-                            activity.startActivity(Intent(action, uri))
-                        }
-                        .wrapContentSize(),
-                    text = "request",
-                )
-            }
-        } else if (state != null) {
+        if (state != null) {
             TreeScreen(
                 state = state,
                 toParent = {
